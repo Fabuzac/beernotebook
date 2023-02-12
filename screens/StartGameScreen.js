bf6dd0749b47d7d44cef9f813d91851502e406d1
Fabuzac <fabuzac.novenaire@hotmail.com>
2023-02-12 18:36:02 +0100

    Home: center buttons and input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,8 +11,16 @@ function StartGameScreen() {
         autoCapitalize='none'
         keyboardType='phone-pad'
       />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+       
     </View>
   );
 }
@@ -22,6 +30,8 @@ export default StartGameScreen;
 const styles = StyleSheet.create({
 
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -44,6 +54,13 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex:1,
   }
 
 });
